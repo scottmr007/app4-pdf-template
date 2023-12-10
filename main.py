75c11a5e713d89bf6fcd9334ffde851c075bbd53
scottmr007 <mrscott@tutamail.com>
2023-12-10 16:19:25 -0500

    Day 24: Add more pages for each topic

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
     pdf.add_page()
 
-    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_font(family="Times", style="B", size=18)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
+    
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
 
 
 pdf.output("output.pdf")
