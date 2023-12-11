502af0e35fd8859e254480b52529186a0c96b0da
scottmr007 <mrscott@tutamail.com>
2023-12-11 15:20:44 -0500

    Day 24: Added multiple lines

--- main.py
+++ main.py
@@ -14,22 +14,29 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=18)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+       
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
     
     #Footer
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
-    
+      
     for i in range(row["Pages"] - 1):
         pdf.add_page()
-        
+               
     #Footer
     pdf.ln(277)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+    
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
+    
+    
 
 
 pdf.output("output.pdf")
